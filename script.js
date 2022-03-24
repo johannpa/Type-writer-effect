7f96002db7c8e78906b682ad2675bc754b42bf18
johannpa <johann_paque@hotmail.fr>
2022-03-24 08:36:21 +0100

    add test for the function

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const TypeWriter = function(txtElement, words, wait = 3000) {
-    this.txtElement = txtElement;
-    this.words = words;
-    this.txt = '';
-    this.wordIndex = 0;
-    this.wait = parseInt(wait, 10);
-    this.type();
-    this.isDeleting = false;
-}
+      this.txtElement = txtElement;
+      this.words = words;
+      this.txt = '';
+      this.wordIndex = 0;
+      this.wait = parseInt(wait, 10);
+      this.type();
+      this.isDeleting = false;
+    }
 
 // Type Method
+TypeWriter.prototype.type = function() {
+    console.log('Hello');
+
+    setTimeout(() => this.type(), 500)
+}
 
 // Init On DOM Load
 document.addEventListener('DOMContentLoaded', init);
